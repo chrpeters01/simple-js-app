2c92bfb405ff8c2a90176d39c11e412f7fb40f17
chrpeters01 <chrisbpeters01@gmail.com>
2023-08-07 12:21:11 -0500

    Update script.js
    added semi-colon

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ let pokemonList = [
     { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
     { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
     { name: "Metapod", height: 0.7, types: ["bug"] },
-  ]
+  ];
