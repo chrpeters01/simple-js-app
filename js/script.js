89c1bc7d6af871daf99ed7cd5c763b192b999236
chrpeters01 <chrisbpeters01@gmail.com>
2023-08-15 11:50:43 -0500

    Update script.js
    Edited list so each appear on new lines

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-    { name: "Metapod", height: 0.7, types: ["bug"] },
-  ];
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Charizard",
+      height: "1.7",
+      types: ["fire", "flying"]
+    },
+    {
+      name: "Metapod",
+      height: 0.7,
+      types: ["bug"]
+    }
+   ];
+  
