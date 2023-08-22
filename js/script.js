798be2db95e1a07d1b72c4fd562f27c6b27cb336
chrpeters01 <chrisbpeters01@gmail.com>
2023-08-22 12:47:19 -0500

    Update script.js
    Updated document.write

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const pokemonList = [
+let pokemonList = [
   {
     name: "Bulbasaur",
     height: 0.7,
@@ -19,10 +19,7 @@ const pokemonList = [
   for (let i = 0; i < pokemonList.length; i++){
     if (pokemonList[i].height <1.0 && pokemonList[i].height >1.5)
     console.log(pokemonList[i].name + " Wow, that's big!");
-    document.write(pokemonList)
+    document.write(pokemonList[i].name);
   }
 
   
-
-  
-
