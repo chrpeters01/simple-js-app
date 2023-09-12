26df67fec5b700792616cf0995d1c47aaf2dde3f
chrpeters01 <chrisbpeters01@gmail.com>
2023-09-12 12:43:20 -0500

    Update script.js
    moved functions above return and separated add and getAll

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
-let pokemonRepository = (function(){
-  let pokemonList=[
-        
-        {
-            name: 'Bulbasaur',
-            height: 0.7,
-            types: ['Grass' , 'Poison']
-    
-        },
-        {
-            name: 'Charizard',
-            height: 1.7,
-            types: ['Fire' , 'Flying']
-    
-        },
-        {
-            name: 'Metapod',
-            height: 0.7,
-            types: ['Bug']
-    
-        }
-    ];
-    
-    return {
-      add: function(pokemon) {
-        pokemonList.push(pokemon);
-      },
-      getAll: function() {
-        return pokemonList;
-      }
-    };
- })();
-      
-pokemonRepository.getAll().forEach(function(pokemon){
-document.write(`<p> ${pokemon.name} : (types: ${pokemon.types}) / (height: ${pokemon.height})`);
+let pokemonRepository = (function () {
+  let pokemonList = [
+
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['Grass', 'Poison']
+
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['Fire', 'Flying']
+
+    },
+    {
+      name: 'Metapod',
+      height: 0.7,
+      types: ['Bug']
+
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(`<p> ${pokemon.name} : (type: ${pokemon.types}) / (height: ${pokemon.height})`);
 })
