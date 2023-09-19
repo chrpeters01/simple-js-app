109d8d4f8837d0babcb6a68c393adb6b7bdd7ceb
chrpeters01 <chrisbpeters01@gmail.com>
2023-09-19 12:58:28 -0500

    Update script.js
    Changed forEach loop / included addListItem function

--- js/script.js
+++ js/script.js
@@ -29,12 +29,24 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(`<p> ${pokemon.name} : (type: ${pokemon.types}) / (height: ${pokemon.height})`);
-})
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+ 
+});
